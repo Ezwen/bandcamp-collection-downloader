39721530ae01dc1b800713f0f91b68b9219d6dac
Gwendal <gwendal@bousse.fr>
2020-04-16 16:11:28 +0200

    More refactoring: use Args data class as parameter

--- src/main/kotlin/bandcampcollectiondownloader/BandcampCollectionDownloader.kt
+++ src/main/kotlin/bandcampcollectiondownloader/BandcampCollectionDownloader.kt
@@ -16,38 +16,37 @@ import java.util.*
 object BandcampCollectionDownloader {
 
 
-
     /**
      * Core function called from the main
      */
-    fun downloadAll(cookiesFile: Path?, bandcampUser: String, downloadFormat: String, downloadFolder: Path, retries: Int, timeout: Int, ignoreFailedAlbums: Boolean) {
+    fun downloadAll(args: Args) {
 
         val gson = Gson()
 
         val cookies =
 
-                if (cookiesFile != null) {
+                if (args.pathToCookiesFile != null) {
                     // Parse JSON cookies (obtained with "Cookie Quick Manager" Firefox addon)
-                    println("Loading provided cookies file: $cookiesFile")
-                    CookiesManagement.retrieveCookiesFromFile(cookiesFile, gson)
+                    println("Loading provided cookies file: $args.cookiesFile")
+                    CookiesManagement.retrieveCookiesFromFile(args.pathToCookiesFile!!, gson)
                 } else {
                     // Try to find cookies stored in default firefox profile
                     println("No provided cookies file, using Firefox cookies.")
                     CookiesManagement.retrieveFirefoxCookies()
                 }
 
-        val collection = BandcampAPIHelper.getCollection(cookies, timeout, bandcampUser, gson)
+        val collection = BandcampAPIHelper.getCollection(cookies, args.timeout, args.bandcampUser, gson)
 
 
         // For each download page
         for ((saleItemId, redownloadUrl) in collection) {
-            manageDownloadPage(saleItemId, redownloadUrl, cookies, gson, timeout, downloadFormat, downloadFolder, retries, ignoreFailedAlbums)
+            manageDownloadPage(saleItemId, redownloadUrl, cookies, gson, args)
         }
     }
 
-    private fun manageDownloadPage(saleItemId: String, redownloadUrl: String, cookies: Map<String, String>, gson: Gson, timeout: Int, downloadFormat: String, downloadFolder: Path, retries: Int, ignoreFailedAlbums: Boolean) {
+    private fun manageDownloadPage(saleItemId: String, redownloadUrl: String, cookies: Map<String, String>, gson: Gson, args: Args) {
 
-        val cacheFile = downloadFolder.resolve("bandcamp-collection-downloader.cache")
+        val cacheFile = args.pathToDownloadFolder.resolve("bandcamp-collection-downloader.cache")
         val cache = loadCache(cacheFile).toMutableList()
 
         // less Bandcamp-intensive way of checking already downloaded things
@@ -57,7 +56,7 @@ object BandcampCollectionDownloader {
         }
 
         println("Getting data from item page ($redownloadUrl)…")
-        val digitalItem = BandcampAPIHelper.getDataBlobFromDownloadPage(redownloadUrl, cookies, gson, timeout)
+        val digitalItem = BandcampAPIHelper.getDataBlobFromDownloadPage(redownloadUrl, cookies, gson, args.timeout)
 
         // If null, then the download page is simply invalid and not usable anymore, therefore it can be added to the cache
         if (digitalItem == null) {
@@ -83,7 +82,7 @@ object BandcampCollectionDownloader {
         val releaseDate = digitalItem.package_release_date
         val releaseYear = releaseDate.subSequence(7, 11)
         val isSingleTrack: Boolean = digitalItem.download_type == "t"
-        val downloadUrl = digitalItem.downloads[downloadFormat]?.get("url").orEmpty()
+        val downloadUrl = digitalItem.downloads[args.audioFormat]?.get("url").orEmpty()
         if (downloadUrl.isEmpty()) {
             throw BandCampDownloaderError("No URL found (is the download format correct?)")
         }
@@ -95,18 +94,18 @@ object BandcampCollectionDownloader {
 
         // Prepare artist and album folder
         val albumFolderName = "$releaseYear - $albumtitle"
-        val artistFolderPath = Paths.get("$downloadFolder").resolve(artist)
+        val artistFolderPath = Paths.get("${args.pathToDownloadFolder}").resolve(artist)
         val albumFolderPath = artistFolderPath.resolve(albumFolderName)
 
         // Download album, with as many retries as configured
-        val attempts = retries + 1
+        val attempts = args.retries + 1
         for (i in 1..attempts) {
             if (i > 1) {
-                println("Retrying download (${i - 1}/$retries).")
+                println("Retrying download (${i - 1}/${args.retries}).")
                 sleep(1000)
             }
             try {
-                val downloaded = downloadAlbum(artistFolderPath, albumFolderPath, downloadUrl, cookies, gson, isSingleTrack, artid, timeout)
+                val downloaded = downloadAlbum(artistFolderPath, albumFolderPath, downloadUrl, cookies, gson, isSingleTrack, artid, args.timeout)
                 if (downloaded) {
                     println("done.")
                 } else {
@@ -120,10 +119,10 @@ object BandcampCollectionDownloader {
             } catch (e: Throwable) {
                 println("""Error while downloading: "${e.javaClass.name}: ${e.message}".""")
                 if (i == attempts) {
-                    if (ignoreFailedAlbums) {
-                        println("Could not download release after $retries retries.")
+                    if (args.ignoreFailedAlbums) {
+                        println("Could not download release after ${args.retries} retries.")
                     } else {
-                        throw BandCampDownloaderError("Could not download release after $retries retries.")
+                        throw BandCampDownloaderError("Could not download release after ${args.retries} retries.")
                     }
                 }
             }
--- src/main/kotlin/bandcampcollectiondownloader/Main.kt
+++ src/main/kotlin/bandcampcollectiondownloader/Main.kt
@@ -2,40 +2,6 @@ package bandcampcollectiondownloader
 
 import BandCampDownloaderError
 import picocli.CommandLine
-import java.nio.file.Path
-import java.nio.file.Paths
-
-
-data class Args(
-        @CommandLine.Parameters(arity = "1..1",
-                description = ["The bandcamp user account from which all albums must be downloaded."])
-        var bandcampUser: String = "",
-
-        @CommandLine.Option(names = ["--cookies-file", "-c"], required = false,
-                description = ["A JSON file with valid bandcamp credential cookies.", """"Cookie Quick Manager" can be used to obtain this file after logging into bandcamp.""", "(visit https://addons.mozilla.org/en-US/firefox/addon/cookie-quick-manager/).", "If no cookies file is provided, cookies from the local Firefox installation are used (Windows and Linux only)."])
-        var pathToCookiesFile: Path? = null,
-
-        @CommandLine.Option(names = ["--audio-format", "-f"], required = false,
-                description = ["The chosen audio format of the files to download (default: \${DEFAULT-VALUE}).", "Possible values: flac, wav, aac-hi, mp3-320, aiff-lossless, vorbis, mp3-v0, alac."])
-        var audioFormat: String = "vorbis",
-
-        @CommandLine.Option(names = ["--download-folder", "-d"], required = false,
-                description = ["The folder in which downloaded albums must be extracted.", "The following structure is considered: <pathToDownloadFolder>/<artist>/<year> - <album>.", "(default: current folder)"])
-        var pathToDownloadFolder: Path = Paths.get("."),
-
-        @CommandLine.Option(names = ["-h", "--help"], usageHelp = true, description = ["Display this help message."])
-        var help: Boolean = false,
-
-        @CommandLine.Option(names = ["-r", "--retries"], usageHelp = false, description = ["Amount of retries when downloading an album (default: 3)."])
-        var retries: Int = 3,
-
-        @CommandLine.Option(names = ["-t", "--timeout"], usageHelp = false, description = ["Timeout in ms before giving up an HTTP connection (default: 50000)."])
-        var timeout: Int = 50000,
-
-        @CommandLine.Option(names = ["-e", "--skip-failed-albums"], description = ["Skip albums that fail to download after the specified number of retries."])
-        var ignoreFailedAlbums: Boolean = false
-
-)
 
 
 fun main(args: Array<String>) {
@@ -61,15 +27,9 @@ fun main(args: Array<String>) {
 
     // Else, parse arguments and run
     else {
-        val bandcampUser = parsedArgs.bandcampUser
-        val cookiesFile = parsedArgs.pathToCookiesFile
-        val downloadFormat = parsedArgs.audioFormat
-        val downloadFolder = parsedArgs.pathToDownloadFolder
-        val retries = parsedArgs.retries
-        val timeout = parsedArgs.timeout
-        val ignoreFailedAlbums = parsedArgs.ignoreFailedAlbums
+
         try {
-            BandcampCollectionDownloader.downloadAll(cookiesFile, bandcampUser, downloadFormat, downloadFolder, retries, timeout, ignoreFailedAlbums)
+            BandcampCollectionDownloader.downloadAll(parsedArgs)
         } catch (e: BandCampDownloaderError) {
             System.err.println("ERROR: ${e.message}")
         }
