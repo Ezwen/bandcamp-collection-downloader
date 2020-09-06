28a537bb809d9d1174e4f01ff152c686c9c4c077
Colin <colin.diesh@gmail.com>
2020-09-06 12:57:57 -0400

    Add to args.kt

--- src/main/kotlin/bandcampcollectiondownloader/Args.kt
+++ src/main/kotlin/bandcampcollectiondownloader/Args.kt
@@ -10,7 +10,13 @@ data class Args(
         var bandcampUser: String = "",
 
         @CommandLine.Option(names = ["--cookies-file", "-c"], required = false,
-                description = ["A JSON file with valid bandcamp credential cookies.", """"Cookie Quick Manager" can be used to obtain this file after logging into bandcamp.""", "(visit https://addons.mozilla.org/en-US/firefox/addon/cookie-quick-manager/).", "If no cookies file is provided, cookies from the local Firefox installation are used (Windows and Linux only)."])
+                description = ["A JSON or txt file with valid bandcamp credential cookies.",
+                            """"Cookie Quick Manager" can be used to obtain a JSON file after logging into bandcamp.""",
+                            "(visit https://addons.mozilla.org/en-US/firefox/addon/cookie-quick-manager/)",
+                            """"or the "cookies.txt" extension"""",
+                            "(https://chrome.google.com/webstore/detail/cookiestxt/njabckikapfpffapmjgojcnbfjonfjfg?hl=en)",
+                            "can be used to obtain a cookies.txt file on Chrome.",
+                            "If no cookies file is provided, cookies from the local Firefox installation are used (Windows and Linux) only)."])
         var pathToCookiesFile: Path? = null,
 
         @CommandLine.Option(names = ["--audio-format", "-f"], required = false,
