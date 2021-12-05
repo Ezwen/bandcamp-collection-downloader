03d014bac568ff2d766b9ef2bebed87d25fc7ffc
Ezwen <gwendal@bousse.fr>
2021-12-05 09:55:49 +0100

    Release v2021-12-05

--- src/main/kotlin/bandcampcollectiondownloader/core/Constants.kt
+++ src/main/kotlin/bandcampcollectiondownloader/core/Constants.kt
@@ -3,7 +3,7 @@ package bandcampcollectiondownloader.core
 object Constants {
 
     const val LINESIZE: Int = 130
-    const val VERSION: String = "v2020-12-15"
+    const val VERSION: String = "v2021-12-05"
     val UNICODE_CHARS_REPLACEMENTS = hashMapOf<Char, Char>(
             ':' to '꞉',
             '/' to '／',
